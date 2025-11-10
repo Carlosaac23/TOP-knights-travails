04a96b95232f3c6699baa09a54c0a949fa21426b
Carlos <carlosaac232001@gmail.com>
2025-11-10 15:05:43 -0500

    feat: - fix queue const - add movements array which are the movements the knight can do - create a visited set to keep track of visited positions - check if the current position is the end position using tupleToString helper function - add new positions to visited set and queue - shift the queue after processing current position

--- knights.js
+++ knights.js
@@ -1,5 +1,51 @@
+import { tupleToString } from './helpers';
+
 export default function knightsMoves(start, end) {
   // Each knight move is represented as a tuple [x, y]
-  const queue = [[start, 0]]; // [position, moves]
+  const queue = [{ position: start, path: [start] }];
   const visited = new Set();
+  const movements = [
+    [+2, +1],
+    [+2, -1],
+    [-2, +1],
+    [-2, -1],
+    [+1, +2],
+    [+1, -2],
+    [-1, +2],
+    [-1, -2],
+  ];
+
+  while (queue.length > 0) {
+    const currentPosition = queue[0].position;
+    const currentPath = queue[0].path;
+
+    if (tupleToString(currentPosition) === tupleToString(end)) {
+      return currentPath;
+    }
+
+    for (const [dx, dy] of movements) {
+      const newX = currentPosition[0] + dx;
+      const newY = currentPosition[1] + dy;
+
+      if (newX > 7 || newX < 0) {
+        continue;
+      }
+
+      if (newY > 7 || newY < 0) {
+        continue;
+      }
+
+      if (visited.has(tupleToString([newX, newY]))) {
+        continue;
+      }
+
+      visited.add(tupleToString([newX, newY]));
+      queue.push({
+        position: [newX, newY],
+        path: [...currentPath, [newX, newY]],
+      });
+    }
+
+    queue.shift();
+  }
 }
