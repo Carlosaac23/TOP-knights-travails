82fc7987441722da6ca15543b19ca92f6e845ae5
Carlos <carlosaac232001@gmail.com>
2025-11-10 15:28:15 -0500

    chore: add comments to improve readability

--- knights.js
+++ knights.js
@@ -3,7 +3,9 @@ import { tupleToString } from './helpers';
 export default function knightsMoves(start, end) {
   // Each knight move is represented as a tuple [x, y]
   const queue = [{ position: start, path: [start] }];
+  // Set to keep track of visited positions
   const visited = new Set();
+  // Movemments a knight can make
   const movements = [
     [+2, +1],
     [+2, -1],
@@ -37,14 +39,17 @@ export default function knightsMoves(start, end) {
     const currentPosition = queue[0].position;
     const currentPath = queue[0].path;
 
+    // Check if we reached the end position
     if (tupleToString(currentPosition) === tupleToString(end)) {
       return currentPath;
     }
 
+    // Explore all possible knight moves from the current position
     for (const [dx, dy] of movements) {
       const newX = currentPosition[0] + dx;
       const newY = currentPosition[1] + dy;
 
+      // Check if the new position is within bounds
       if (newX > 7 || newX < 0) {
         continue;
       }
@@ -53,10 +58,12 @@ export default function knightsMoves(start, end) {
         continue;
       }
 
+      // Check if the new position has already been visited
       if (visited.has(tupleToString([newX, newY]))) {
         continue;
       }
 
+      // Add the new position to the queue and mark it as visited
       visited.add(tupleToString([newX, newY]));
       queue.push({
         position: [newX, newY],
